2bfb7eac93d563e5b34d7a7ac8d27007eb426c0f
KoSChicken <kosc21@outlook.com>
2023-05-08 17:50:13 +0800

    update

---
+++ src/io/github/koschicken/easy/ContainsDuplicate.java
@@ -0,0 +1,26 @@
+package io.github.koschicken.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums1));
+        int[] nums2 = {1, 2, 3, 4};
+        System.out.println(containsDuplicate(nums2));
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(containsDuplicate(nums3));
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashSet = new HashSet<>();
+        for (int i : nums) {
+            if (!hashSet.add(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/io/github/koschicken/easy/MajorityElement.java
@@ -0,0 +1,30 @@
+package io.github.koschicken.easy;
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        int[] nums1 = {3, 2, 3};
+        System.out.println(majorityElement(nums1));
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums2));
+        int[] nums3 = {-1, 1, 1, 1, 2, 1};
+        System.out.println(majorityElement(nums3));
+    }
+
+    private static int majorityElement(int[] nums) {
+        int k = nums[0];
+        int count = 1;
+        for (int i : nums) {
+            if (i == k) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    count = 1;
+                    k = i;
+                }
+            }
+        }
+        return k;
+    }
+}
