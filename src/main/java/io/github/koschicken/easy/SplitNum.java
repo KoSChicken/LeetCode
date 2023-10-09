b8763a1e24d2c4ccdfe6dd65377168d34a58c68e
KoSChicken <kosc21@outlook.com>
2023-10-09 19:58:56 +0800

    2578. 最小和分割

---
+++ src/main/java/io/github/koschicken/easy/SplitNum.java
@@ -0,0 +1,50 @@
+package io.github.koschicken.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SplitNum {
+
+    @Test
+    void solution() {
+        assertEquals(59, splitNum(4325));
+        assertEquals(75, splitNum(687));
+    }
+
+    public int splitNum(int num) {
+        int[] array = toArray(num);
+        Arrays.sort(array);
+        int n = array.length;
+        int[] a = new int[(n + 1) / 2];
+        int[] b = new int[n / 2];
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                a[i / 2] = array[i];
+            } else {
+                b[i / 2] = array[i];
+            }
+        }
+        return toNum(a) + toNum(b);
+    }
+
+    private int[] toArray(int num) {
+        int n = String.valueOf(num).length();
+        int[] arr = new int[n];
+        while (num > 0) {
+            arr[--n] = num % 10;
+            num /= 10;
+        }
+        return arr;
+    }
+
+    private int toNum(int[] arr) {
+        int num = 0;
+        for (int i : arr) {
+            num = num * 10 + i;
+        }
+        return num;
+    }
+}
